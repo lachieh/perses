4a050024770070a29f0e54fe6e895b24e8d39f05
Lachlan Heywood <lachieh@users.noreply.github.com>
2025-09-19 19:30:05 -0400

    [ENHANCEMENT] Add reverse proxy support
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- scripts/api_backend_dev.sh
+++ scripts/api_backend_dev.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-## /!\ This file must be used at the root of the perses project
+## /!\ This file must be used from within the perses project
 ## This script automates the steps for running the api backend server during
 ## development.
 
-echo ">> build the api server"
-make build-api
+WORKSPACE_ROOT=$(git rev-parse --show-toplevel)
 
-if [[ $1 == "--e2e" ]]; then
-  # deactivate the permission because e2e tests doesn't support yet the JWT cookies
-  previous_file="./dev/config.previous.yaml"
-  config_file="./dev/config.yaml"
-  cp ${config_file} ${previous_file}
-  sed 's/enable_auth: true/enable_auth: false/g' ${previous_file} >${config_file}
-  rm ${previous_file}
-fi
+echo ">> build the api server"
+make -C "${WORKSPACE_ROOT}" build-api
+
+# if [[ $1 == "--e2e" ]]; then
+#   # deactivate the permission because e2e tests doesn't support yet the JWT cookies
+#   previous_file="${WORKSPACE_ROOT}/dev/config.previous.yaml"
+#   config_file="${WORKSPACE_ROOT}/dev/config.yaml"
+#   cp "${config_file}" "${previous_file}"
+#   sed 's/enable_auth: true/enable_auth: false/g' "${previous_file}" >"${config_file}"
+#   rm "${previous_file}"
+# fi
 
 # Run backend server
 echo ">> start the api server"
-echo '>> Log in with user: `admin` and password: `password`'
-./bin/perses --config ./dev/config.yaml --log.level=debug
+echo '>> Log in with user: "admin" and password: "password"'
+"${WORKSPACE_ROOT}"/bin/perses --config "${WORKSPACE_ROOT}"/dev/config.yaml --log.level=debug
